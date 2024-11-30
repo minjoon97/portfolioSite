9de7436932e491353a2c73dd96e45c73ddea2383
kimminjoon <annawa6@naver.com>
2024-11-30 17:09:53 +0900

    ✨feat: about test

--- src/components/AboutModal/AboutModal.tsx
+++ src/components/AboutModal/AboutModal.tsx
@@ -18,7 +18,23 @@ const AboutModal: React.FC<AboutModalProps> = ({
         }}
         $modalState={modalState}
       ></Background>
-      <Wrapper $modalState={modalState}></Wrapper>
+      <Wrapper $modalState={modalState}>
+        <p>about 710minjoon</p>
+        <p>
+          My journey as a developer began as an intern in a development team
+          during my final semester, where I gained practical experience through
+          intranet maintenance and building automation processes. Later,
+          recognizing the importance of UI/UX in frontend development, I worked
+          in an agency's design team, developing a comprehensive perspective
+          that bridges design and development through more than 20 projects.
+          Currently, I'm gaining team project experience at Programmers
+          DevCourse, and I've created this simple and intuitive portfolio that
+          encompasses both my development and design capabilities. Through
+          minimal design using geometric shapes and interactive elements
+          implemented in React, you can experience my pursuit of clean code and
+          user-friendly design principles.
+        </p>
+      </Wrapper>
     </>
   );
 };
