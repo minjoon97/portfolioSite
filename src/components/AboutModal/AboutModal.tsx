e6f89ea8a280a35e5376c06b9ef4acc2a98e7df9
Minjoon Kim <annawa678@gmail.com>
2024-12-04 20:26:29 +0900

    ⛱feat : 241204 modal수정

--- src/components/AboutModal/AboutModal.tsx
+++ src/components/AboutModal/AboutModal.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import { Wrapper, Background } from "./AboutModalCss";
+import { Wrapper, Background, CloseBtn, IconWrapper } from "./AboutModalCss";
 
 interface AboutModalProps {
   modalState: boolean;
@@ -12,14 +12,16 @@ const AboutModal: React.FC<AboutModalProps> = ({
 }) => {
   return (
     <>
-      <Background
-        onClick={() => {
-          setModalState(false);
-        }}
-        $modalState={modalState}
-      ></Background>
+      <Background $modalState={modalState}></Background>
       <Wrapper $modalState={modalState}>
-        <p>about 710minjoon</p>
+        <CloseBtn
+          onClick={() => {
+            setModalState(false);
+          }}
+        >
+          <img src="closeicon.svg" alt=""></img>
+        </CloseBtn>
+        <p>about</p>
         <p>
           My journey as a developer began as an intern in a development team
           during my final semester, where I gained practical experience through
@@ -28,12 +30,18 @@ const AboutModal: React.FC<AboutModalProps> = ({
           in an agency's design team, developing a comprehensive perspective
           that bridges design and development through more than 20 projects.
           Currently, I'm gaining team project experience at Programmers
-          DevCourse, and I've created this simple and intuitive portfolio that
-          encompasses both my development and design capabilities. Through
-          minimal design using geometric shapes and interactive elements
-          implemented in React, you can experience my pursuit of clean code and
-          user-friendly design principles.
+          DevCourse, and I've created this{` `}
+          <span>simple and intuitive portfolio</span> that encompasses both my
+          development and design capabilities. Through minimal design using
+          geometric shapes and interactive elements implemented in React, you
+          can experience my pursuit of clean code and user-friendly design
+          principles.
         </p>
+        <p>Copyright ⓒ 2024 Minjoon Kim. All Right Reserved</p>
+        <IconWrapper>
+          <img src="reacticon.svg" alt=""></img>
+          <img src="typescripticon.svg" alt=""></img>
+        </IconWrapper>
       </Wrapper>
     </>
   );
--- src/components/AboutModal/AboutModalCss.ts
+++ src/components/AboutModal/AboutModalCss.ts
@@ -3,29 +3,41 @@ import styled from "styled-components";
 //모달
 export const Wrapper = styled.div<{ $modalState: boolean }>`
   width: 90%;
-  max-width: 800px;
-  height: 500px;
+  max-width: 600px;
   display: ${(props) => (props.$modalState === true ? "block" : "none")};
   position: fixed;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -40%);
+  transform: translate(-50%, -50%);
   z-index: 9999;
-  padding: 1.5rem;
   color: white;
+  font-family: "Inter", sans-serif;
 
   & > p:nth-of-type(1) {
     font-size: 40px;
     font-weight: 600;
-    padding-bottom: 40px;
+    padding-bottom: 50px;
   }
 
   & > p:nth-of-type(2) {
     font-size: 14px;
-    padding-bottom: 10px;
+    padding-bottom: 30px;
     color: rgba(255, 255, 255, 0.8);
+    font-weight: 100;
+    letter-spacing: 1px;
+    line-height: 20px;
+
+    & > span {
+      font-weight: 300;
+      color: white;
+    }
+  }
+
+  & > p:nth-of-type(3) {
+    font-size: 12px;
+    padding-bottom: 30px;
+    color: rgba(255, 255, 255, 0.5);
     font-weight: 200;
-    line-height: 18px;
   }
 `;
 
@@ -41,4 +53,48 @@ export const Background = styled.div<{ $modalState: boolean }>`
   visibility: ${(props) => (props.$modalState === true ? "visible" : "hidden")};
   opacity: ${(props) => (props.$modalState === true ? 0.95 : 0)};
   z-index: 9999;
+`;
+
+export const CloseBtn = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.2s;
+
+  &:hover {
+    border: 1px solid rgba(255, 255, 255, 0.6);
+    cursor: pointer;
+  }
+
+  & > img {
+    width: 34px;
+    opacity: 0.6;
+    transition: 0.2s;
+  }
+
+  &:hover > img {
+    width: 34px;
+    opacity: 0.8;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  & > img:nth-of-type(1) {
+    width: 40px;
+  }
+
+  & > img:nth-of-type(2) {
+    width: 30px;
+  }
 `;
